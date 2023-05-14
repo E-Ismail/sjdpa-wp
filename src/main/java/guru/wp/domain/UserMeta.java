22ccb3717d47f24b082849cbfde2f597514e09b3
echcherrate.ismail <echcherrate.ismail@gmail.com>
2023-05-14 19:28:20 +0200

    Section 21: Legacy Database Mapping

---
+++ src/main/java/guru/wp/domain/UserMeta.java
@@ -0,0 +1,61 @@
+package guru.wp.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+/**
+ * @author E.I.
+ * <p>
+ * {@code @Date}  5/14/2023
+ */
+@Entity
+@Table(name = "wp_usermeta")
+public class UserMeta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "umeta_id")
+    private Long id;
+    private Long userId;
+    @NotNull
+    @Size(max = 255)
+    @Column(length = 255)
+    private String metaKey;
+
+    @Lob
+    @Column(columnDefinition = "longtext")
+    private String metaValue;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getMetaKey() {
+        return metaKey;
+    }
+
+    public void setMetaKey(String metaKey) {
+        this.metaKey = metaKey;
+    }
+
+    public String getMetaValue() {
+        return metaValue;
+    }
+
+    public void setMetaValue(String metaValue) {
+        this.metaValue = metaValue;
+    }
+}
